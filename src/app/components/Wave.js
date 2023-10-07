223dedc69760b221cbdc84be4b2032fd5ba6abdd
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-10-07 17:29:00 -0500

    initial

---
+++ src/app/components/Wave.js
@@ -0,0 +1,11 @@
+export default function Wave() {
+  return (
+    <div class="boxContainer">
+      <div class="box box1"></div>
+      <div class="box box2"></div>
+      <div class="box box3"></div>
+      <div class="box box4"></div>
+      <div class="box box5"></div>
+    </div>
+  );
+}
