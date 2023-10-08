01f19e2421fc055d5d65544c7b173612b61be749
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-10-07 20:48:35 -0500

    redesigned new page

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,35 +1,35 @@
 "use client";
+import { Fragment, useState } from "react";
 
-export default function Chat() {
+export default function Example() {
   return (
     <>
-      <div className="fixed h-full w-full z-10">
-        <video
-          autoPlay
-          loop
-          type="video/mp4"
-          src="/clouds.mp4"
-          className="h-[110%] w-[110%] blur-sm"
-        />
-      </div>
-      <div className="fixed w-screen h-screen z-20 bg-white bg-opacity-20"></div>
-      <div className="h-screen w-screen flex items-center z-0">
-        <div className="w-fit mx-auto h-1/4 flex items-center gap-4 z-20">
-          <div className="shadow-lg flex flex-col p-4 h-full rounded-lg gap-3 bg-white bg-opacity-50 ">
-            <h1 className="text-xl font-semibold">
-              Provide a practice scenario
-            </h1>
-            <textarea className="w-full border rounded-md border-gray-300"></textarea>
-            <button className="bg-green-500 hover:bg-green-600 rounded-lg px-4 py-1 duration-200 text-white text-lg font-semibold">
-              Go
-            </button>
+      <main className="bg-gray-700 h-screen flex items-center">
+        <div className="mx-auto space-y-6 w-96">
+          <h1 className="font-serif text-white text-xl text-center">
+            Choose a scenario
+          </h1>
+          <textarea
+            rows={10}
+            placeholder="Enter a practice scenario"
+            className="block rounded-md w-full p-4 bg-gray-800  text-white min-h-96 ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-green-600"
+          />
+          <div className="relative">
+            <div
+              className="absolute inset-0 flex items-center"
+              aria-hidden="true"
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center">
+              <span className="bg-gray-700 px-2 text-sm text-gray-300">Or</span>
+            </div>
           </div>
-          <h1 className="text-lg">or</h1>
-          <button className="bg-green-500 hover:bg-green-600 rounded-lg px-4 py-1 duration-200 text-white text-lg font-semibold">
-            Generate a random one
+          <button className="bg-green-600 px-4 py-2 rounded-md hover:opacity-75 text-white w-full">
+            Generate a random scenario
           </button>
         </div>
-      </div>
+      </main>
     </>
   );
 }
